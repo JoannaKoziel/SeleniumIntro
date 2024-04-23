62355d1478652fbf503a9ee617235316c991ffb0
JoannaKoziel <joannabgajda@gmail.com>
2024-04-23 21:51:36 +0200

    SeleniumIntro

---
+++ src/test/java/tests/FailedLoginTests.java
@@ -0,0 +1,24 @@
+package tests;
+import driver.manager.DriverUtils;
+import org.testng.annotations.Test;
+import page.objects.LoginPage;
+
+import static navigation.ApplicationURLs.LOGIN_URL;
+import static org.testng.AssertJUnit.assertEquals;
+
+public class FailedLoginTests extends TestBase {
+
+    @Test
+    public void asUserTryToLogInWithIncorrectLoginAndPassword() {
+        DriverUtils.navigateToPage(LOGIN_URL);
+        LoginPage loginPage= new LoginPage();
+        loginPage
+                .typeIntoUserNameField("NotExistingLogin")
+                .typeIntoPasswordField("NotProperPassword")
+                .clickOnLoginButton();
+        String warningMessage = loginPage.getWarningMessage();
+
+        assertEquals(warningMessage, "Invalid username or password. Signon failed.");
+    }
+
+}
